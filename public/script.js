701297fa77dc2641a2d904793e8adecfb4c8f0e2
UcheSylvester <okorocode@gmail.com>
2019-07-29 01:59:38 +0100

    working on displaying error

--- public/script.js
+++ public/script.js
@@ -2,7 +2,10 @@
 const textInput = document.getElementById('city');
 
 // selecting the div for errorMessage
-const errorMessage = document.querySelector('.errorMessage')
+const errorMessage = document.querySelector('.errorMessage');
+
+// selecting the weather info div
+const weatherInfo = document.querySelector('.weather-info')
 
 let cityName = textInput.value; // store the value of the input 
 
@@ -30,19 +33,34 @@ form.addEventListener('submit', (e) => {
             let city = document.querySelector('.cityName');
             let celsius = document.querySelector('.celsius');
             let fahrenheit = document.querySelector('.fahrenheit');
-            let errorMessage = document.querySelector('.errorMessage');
 
             if (response.data.city) {
                 city.innerHTML = 'City: ' + response.data.city;
                 celsius.innerHTML = 'Temperature (C): ' + response.data['temperature (C)'];
                 fahrenheit.innerHTML = 'Temperature (F): ' + response.data['temperature (F)'];
             } 
-            else {
-                errorMessage.innerHTML = 'City does not exit in our database';
-            }
+            // else {
+            //     errorMessage.innerHTML = 'City does not exit in our database';
+            // }
         })
         .catch((error) => {
-            console.log(error);
+            // when city doesn't exist in database
+            const errorlog = "This city does not exit in our database"
+
+            // logging the error message from the error response
+            // console.dir(error.response.data);   //check the message properity here
+
+            // check if the message from error response matches message when city doesn't exist
+            if(error.response.data.message === errorlog) {
+                // set errorMessage div to display errorlog
+                errorMessage.innerHTML = errorlog
+                
+                // remove initial weather results
+                weatherInfo.innerHTML = "";
+                
+            } else {
+                console.log('hello...')
+            }
             // errorMessage.innerHTML = 'City does not exit in our database';            
         })
 
